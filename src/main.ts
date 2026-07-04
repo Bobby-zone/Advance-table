efcc7ad8913eca1fc5c65e63c35943b83f0a33cc
mooton <mooton100>
2026-07-04 19:03:28 +0700

    render finish

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import {App, Editor, MarkdownFileInfo, MarkdownView, Notice, Plugin, WorkspaceLeaf} from 'obsidian';
 
-import {TABLE_EDITOR_VIEW, TableEditorView} from './editor/TableEditorView';
 import {TableDocument} from './models/TableDocument';
 import {TableBlockParser} from './parser/TableBlockParser';
 import {TableRenderer} from './renderer/TableRenderer';
@@ -9,10 +8,6 @@ import {TableRenderer} from './renderer/TableRenderer';
 
 export default class TablePlugin extends Plugin {
   async onload() {
-    // register the custom editor view
-    this.registerView(
-        TABLE_EDITOR_VIEW, (leaf: WorkspaceLeaf) => new TableEditorView(leaf));
-
     // find codeblock table
     this.registerMarkdownCodeBlockProcessor(
         'table', async (source, el, ctx) => {
@@ -27,48 +22,7 @@ export default class TablePlugin extends Plugin {
             el.createEl('pre', {text: 'Failed to render table.'});
           }
         });
-
-
-    // command: edit selected table
-    this.addCommand({
-      id: 'edit-table',
-      name: 'Edit table',
-      editorCallback:
-          async (editor: Editor, _ctx: MarkdownView|MarkdownFileInfo) => {
-            const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-
-            const selection = editor.getSelection().trim();
-
-            let model;
-
-            try {
-              model = TableBlockParser.parse(selection);
-            } catch (err) {
-              console.error(err);
-              new Notice('Invalid table JSON.');
-              return;
-            }
-
-            const leaf = this.app.workspace.getLeaf(true);
-
-            await leaf.setViewState({type: TABLE_EDITOR_VIEW, active: true});
-
-            const tableView = leaf.view as TableEditorView;
-
-            const document: TableDocument = {
-              editor,
-              from: editor.getCursor('from'),
-              to: editor.getCursor('to'),
-              model,
-              sourceLeaf: view!.leaf
-            };
-
-            await tableView.setTable(document);
-          }
-    });
   }
 
-  onunload() {
-    this.app.workspace.detachLeavesOfType(TABLE_EDITOR_VIEW);
-  }
+  onunload() {}
 }
